6e5c78811ca8afd8ed64309d06e38a5b852177eb
ariaan ahmed <you@example.com>
2023-04-28 13:04:51 +0600

    62-5 React Marquee and Responsive NavBar

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import logo from '../../../assets/logo.png';
 import moment from 'moment';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
-        <div>
+        <Container >
             <div className="text-center">
                 <img src={logo} alt="logo" />
                 <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
-                <p className='text-secondary' style={{fontWeight: '600'}}>{moment().format('dddd, MMMM D, YYYY')}</p>
+                <p className='text-secondary' style={{ fontWeight: '600' }}>{moment().format('dddd, MMMM D, YYYY')}</p>
             </div>
-            <div>
-                <Button variant='danger'>Breaking News</Button>
+            <div className='d-flex'>
+                <Button variant='danger'>Breaking</Button>
+                <Marquee pauseOnHover={true} speed={40} gradient={true} gradientWidth={40}>
+                    I can be a React component, multiple React components, or just some text.
+                </Marquee>
             </div>
-        </div>
+            {/* --------------navbar------------- */}
+
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#pricing">Career</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                <Button variant="secondary">Login</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
     );
 };
 
